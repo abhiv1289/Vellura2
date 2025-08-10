1c9ab2ef4760a54e3708edcdcfa0699a0975105b
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-08-10 13:56:44 +0530

    trail1

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import autoprefixer from "autoprefixer";
-
 export default defineConfig({
-  plugins: [react(), tailwindcss(), autoprefixer()],
+  plugins: [tailwindcss()],
 });
